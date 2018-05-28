e9f22899fb9c7b65dad9e4d4dc3a95a9bd0591e5
PonySchreck <max98tonton@gmail.com>
2018-05-28 19:09:23 +0200

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pythontranslate',
-      version='0.2',
-      description='Translate your stuff free and online with translate.yandex.com',
-      url='https://github.com/Divirad/PythonTranslate',
-      author='Divirad',
-      author_email='divirad@gmx.de',
-      license='MIT',
-      packages=['pythontranslate'],
-      zip_safe=False)
+long_description = "README.md"
+setup(
+    name='pythontranslate',
+    version='0.0.2',
+    author='divirad',
+    author_email='divirad@gmx.de',
+    description='Translate your stuff free and online with translate.yandex.com',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/Divirad/PythonTranslate',
+    license='MIT',
+    packages = find_packages(),
+    zip_safe=False)
